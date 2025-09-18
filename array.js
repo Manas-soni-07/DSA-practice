e78823e23d2eeaf9de1c089de2d0384b1283e103
manas soni <manassoninit07@gmail.com>
2025-09-18 10:49:05 +0530

    practice set day 1 done

--- array.js
+++ array.js
@@ -1,1 +1,71 @@
 console.log("DSA Practice Started 🚀");
+// DAY 1 practice ques 
+
+// Problem 1: Largest number
+// const numbers = [12, 45, 67, 23, 89, 34];
+
+//  let max = numbers[0]
+
+//  for(let i=0;i<=numbers.length-1;i++){
+//     if(numbers[i] > max){
+//         max = numbers[i]
+//     }
+//  }
+//  console.log("largest no is ",max)
+
+// --------------------------------------------------------------------
+
+// Problem 1: smallest number
+
+// const numbers = [12, 45, 67, 23, 89, 34];
+
+// let smallest = numbers[0];
+
+// for (let i = 0; i <= numbers.length - 1; i++) {
+//   if (numbers[i] < smallest) {
+//     smallest = numbers[i];
+//   }
+// }
+//  console.log("smallest no is ",smallest)
+
+//==========----------------------------------------------------------------
+
+// Problem 3: Reverse an array 
+
+// const arr = [1, 2, 3, 4, 5];
+
+// let rever = []
+
+// for(let i=arr.length-1;i>=0;i--){
+//     rever.push(arr[i])
+// }
+
+// console.log("reverse the array",rever)
+//---------------------------------------------------------------------------
+
+// Problem 4: Second Largest number 
+
+// const nums = [12, 45, 67, 23, 89, 34];
+
+// nums.sort((a,b)=>b-a)
+
+// console.log("second largest no. is",nums[1])
+
+//=---------------------------------------------------------------------
+
+// Problem 5: Sum and Average of an array
+
+// const numbers2 = [10, 20, 30, 40, 50]; 
+
+// let  sum = 0 
+// let average = 0 
+
+// for(let i=0;i<=numbers2.length-1;i++){
+//     sum += numbers2[i]
+//     average = sum/numbers2.length
+// }
+
+// console.log("the sum is :-",sum )
+// console.log("the average  is :-",average )
+
+
--- string.js
+++ string.js
@@ -1,1 +1,28 @@
 console.log("string.js start ....")
+
+// Problem 1: Check Palindrome
+
+// const str = "madam";
+
+// let reverse  = ""
+
+// for(let i=str.length-1;i>=0;i--){
+//     reverse +=str[i]
+// }
+
+// let palindrone = str === reverse;
+
+// console.log("Polindorne true or false", palindrone)
+//----------------------------------------------------------------
+
+// Problem 2: Reverse a string 
+
+// const str2 = "hello"; 
+
+// let reverse = " "
+
+// for(let i=str2.length-1;i>=0;i--){
+//     reverse += str2[i]
+// }
+
+// console.log("reverse strings :-" ,reverse)
